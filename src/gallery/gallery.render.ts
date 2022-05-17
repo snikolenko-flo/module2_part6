ea21a77eac401e21b91c3c962a8f41a58a658561
Stanislav <snikolenko@flo.team>
2022-05-17 09:54:15 +0300

    refactor: change gallery render to typescript
    change gallery render to typescript
    
    MODULE-2-PART-1-typescript

--- src/gallery/gallery.render.ts
+++ src/gallery/gallery.render.ts
@@ -1,16 +1,18 @@
 import { GalleryService } from "./gallery.service.js";
 
 export class RenderGalleryHTML {
+    galleryService: GalleryService;
+
     constructor() {
         this.galleryService = new GalleryService();
     }
 
-    renderPagesList(totalNumberOfPages) {
+    renderPagesList(totalNumberOfPages: number) {
         const pages = document.getElementById('pages');
         pages.innerHTML = this.galleryService.wrapNumbersInHtml(totalNumberOfPages);
     }
 
-    renderImages(imagesUrls) {
+    renderImages(imagesUrls: URL[]) {
         const images = document.getElementById('images');
         images.innerHTML = this.galleryService.wrapUrlsInHtml(imagesUrls);
     }
