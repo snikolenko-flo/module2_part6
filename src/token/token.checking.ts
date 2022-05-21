abad5d61fb319571beaf41ea6a8c4235fb8fa8b7
Stanislav <snikolenko@flo.team>
2022-05-21 17:30:38 +0300

    refactor: change token checking to typescript
    change token checking to typescript
    
    MODULE-2-PART-1-typescript

--- src/token/token.checking.ts
+++ src/token/token.checking.ts
@@ -2,24 +2,27 @@ import { UrlManipulationService } from "../services/url-manipulation.service.js"
 import { TokenService } from "./token.service.js";
 
 export class TokenChecking {
+    urlService: UrlManipulationService;
+    tokenService: TokenService;
+
     constructor() {
         this.urlService = new UrlManipulationService();
         this.tokenService = new TokenService();
     }
 
     checkExpireTime() {
-        const timeLeft = this.tokenService.getTimeLeft();
+        const timeLeft: number = this.tokenService.getTimeLeft();
         this.tokenService.setExpireTimer(timeLeft);
     }
 
     checkTokenExists() {
         if (!this.tokenService.tokenExists()) {
-            this.#redirectToLogin();
+            this.redirectToLogin();
         }
     }
 
-    #redirectToLogin() {
-        const pageNumber = this.urlService.getPageNumberFromUrl();
+    private redirectToLogin() {
+        const pageNumber: number = this.urlService.getPageNumberFromUrl();
         window.location.href = `../login.html?${pageNumber}`;
     }
 }
