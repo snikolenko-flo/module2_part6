c7d90be5153e3c50dfaf0951ad3615a3c2aa54c3
Stanislav <snikolenko@flo.team>
2022-05-21 15:22:59 +0300

    refactor: change token manager to typescript
    change token manager to typescript
    
    MODULE-2-PART-1-typescript

--- src/token/token.manager.ts
+++ src/token/token.manager.ts
@@ -2,6 +2,9 @@ import { TokenSetting } from "./token.setting.js";
 import { TokenChecking } from "./token.checking.js";
 
 export class TokenManager {
+    set: TokenSetting;
+    check: TokenChecking;
+
     constructor() {
         this.set = new TokenSetting();
         this.check = new TokenChecking();
