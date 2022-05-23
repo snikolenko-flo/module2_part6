454a7c598b726c4288ba4ea72e270d65bea3c55e
Stanislav <snikolenko@flo.team>
2022-05-23 22:02:46 +0300

    refactor(gallery): create a constant for minutes
    create a constant for minutes
    
    MODULE-2-PART-1-typescript

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,1 +1,2 @@
 export const BASE_URL: string = 'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api';
+export const ONE_MINUTE: number = 60000;
--- src/token/token.setting.ts
+++ src/token/token.setting.ts
@@ -1,12 +1,12 @@
+import { ONE_MINUTE } from "../data/constants.js";
+
 export class TokenSetting {
-    saveToken(token: string){
+    saveToken(token: string) {
         localStorage.setItem('token', token);
     }
 
     setExpireTime() {
-        const oneMinuteInMs: number = 60000;
-        const tenMinutes: number = oneMinuteInMs * 10;
-
+        const tenMinutes: number = ONE_MINUTE * 10;
         const tokenExpireTime: number = Date.now() + tenMinutes;
         localStorage.setItem('tokenExpireTime', tokenExpireTime.toString());
     }
