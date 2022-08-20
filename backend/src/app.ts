0ec24fe9c67f6dbe8cd9b7cdb89b2b5a11044eb2
Stanislav <snikolenko@flo.team>
2022-08-20 18:14:23 +0300

    feat(gallery): add authorization middleware
    add authorization middleware
    
    MODULE-2-PART-3-express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { loginRouter } from './login/router.js';
 import { galleryRouter } from './gallery/router.js';
 import { galleryHtmlRouter } from './gallery/gallery.html.router.js';
+import { checkAuthorization } from './services/auth.service.js';
 
 const hostname = HOST;
 const port = PORT;
@@ -13,7 +14,7 @@ app.use(express.static('built'));
 app.use('/', loginRouter);
 app.use('/login', loginRouter);
 app.use('/gallery.html', galleryHtmlRouter);
-app.use('/gallery', galleryRouter);
+app.use('/gallery', checkAuthorization, galleryRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- backend/src/gallery/handler.ts
+++ backend/src/gallery/handler.ts
@@ -1,14 +1,11 @@
 import { IMAGES_DIR } from '../data/constants.js';
 import { GalleryManager } from './gallery.manager.js';
-import { AuthService } from '../services/auth.service.js';
 import { UrlService } from '../services/url.service.js';
 
 const manager = new GalleryManager();
-const auth = new AuthService();
 const urlService = new UrlService();
 
 export async function getGallery(req, res) {
-  await auth.checkAuthorization(req, res);
 
   const pageNumber = urlService.getPageNumber(req);
   if (isNaN(pageNumber)) return manager.error.sendIsNanError(res);
