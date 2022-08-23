ccbe7dfb647636a936b5394342893a529354eed5
Stanislav <snikolenko@flo.team>
2022-08-23 22:34:08 +0300

    feat(gallery): add log files rotation
    add log files rotation for every 60 minutes
    
    MODULE-2-PART-3-express

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -4,13 +4,43 @@ import * as fs from 'fs';
 import * as os from 'node:os';
 
 export class Logger {
+  fileCreated: Date;
+  fileName: string;
+  directory: string;
+  createLogInterval: number;
+  minutes: number;
+  oneMinuteInMs: number;
+
+  constructor() {
+    this.fileCreated = new Date();
+    this.directory = 'logs';
+    this.fileName = `logs_${this.fileCreated}.txt`;
+    this.minutes = 60;
+    this.oneMinuteInMs = 60000;
+    this.createLogInterval = this.oneMinuteInMs * this.minutes;
+  }
 
   async writeLog(level, message) {
-    let directory = 'logs';
-    if (!fs.existsSync(directory)) {
-      directory = await mkdir('logs', { recursive: true });
+
+    if (!fs.existsSync(this.directory)) {
+      this.directory = await mkdir('logs', {recursive: true});
     }
-    await writeFile(`./${directory}/logs.txt`, level + ': ' + message + os.EOL, { flag: 'a+' });
+
+    if (!fs.existsSync(`./${this.directory}/${this.fileName}`)) {
+      await writeFile(`./${this.directory}/${this.fileName}`, level + ': ' + message + os.EOL, {flag: 'a+'});
+      this.fileCreated = new Date();
+    } else {
+      const date = new Date();
+      const dif = date.getTime() - this.fileCreated.getTime();
+
+      if (dif > this.createLogInterval) {
+        this.fileName = `logs_${date}.txt`;
+        await writeFile(`./${this.directory}/${this.fileName}`, level + ': ' + message + os.EOL, {flag: 'a+'});
+        this.fileCreated = date;
+      } else {
+        await writeFile(`./${this.directory}/${this.fileName}`, level + ': ' + message + os.EOL, {flag: 'a+'});
+      }
+    }
   }
 
   fatal(message) {
