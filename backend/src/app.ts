c966b395ff57accbbabc47565f641d29a20b1bc5
Stanislav <snikolenko@flo.team>
2022-08-31 09:10:40 +0300

    feat(gallery): add an option for image uploading
    add an option for image uploading
    
    MODULE-2-PART-3-express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,20 @@ import { loginRouter } from './login/router.js';
 import { galleryRouter } from './gallery/router.js';
 import { galleryHtmlRouter } from './gallery/gallery.html.router.js';
 import { checkAuthorization } from './services/auth.service.js';
+import multer from 'multer';
+
+const userFolder = './built/backend/images/uploaded_by_user/';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, userFolder);
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 const hostname = HOST;
 const port = PORT;
@@ -15,6 +29,14 @@ app.use('/', loginRouter);
 app.use('/login', loginRouter);
 app.use('/gallery.html', galleryHtmlRouter);
 app.use('/gallery', checkAuthorization, galleryRouter);
+app.post('/upload', upload.single('img'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    res.status = 400;
+    res.send({error: 'Upload a file please'});
+    res.end();
+  }
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
