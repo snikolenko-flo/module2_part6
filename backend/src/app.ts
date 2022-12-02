c7c812bb1182c7552926771e7b7f3bb2f860e050
Stanislav <snikolenko@flo.team>
2022-12-02 11:58:43 +0400

    feature(gallery): configure cors without cors npm
    configure cors without cors npm
    
    MODULE-2-PART-5-auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ import { DbService } from './services/db-service.js';
 dotenv.config();
 import './login/auth.js';
 import passport from 'passport';
-import cors from 'cors';
+import { cors } from './services/cors.js';
 
 const imagesDir = process.env.IMAGES_DIR;
 const mongoUrl = process.env.MONGO_URL;
@@ -17,28 +17,16 @@ const port = process.env.PORT;
 const dbService = new DbService();
 dbService.startDb(imagesDir, mongoUrl);
 
-const whitelist = ['http://127.0.0.1:3000', 'http://localhost:3000'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
 const app = express();
 
 app.use(express.json());
 app.use(express.static('built'));
-app.use(cors());
-app.use('/', loginRouter);
-app.use('/signup', signUpRouter);
-app.use('/login', loginRouter);
-app.use('/gallery.html', galleryHtmlRouter);
-app.use('/gallery', cors(corsOptions), passport.authenticate('jwt', { session: false }), galleryRouter);
-app.use('/upload', cors(corsOptions), passport.authenticate('jwt', { session: false }), galleryRouter);
+app.use('/', cors, loginRouter);
+app.use('/signup', cors, signUpRouter);
+app.use('/login', cors, loginRouter);
+app.use('/gallery.html', cors, galleryHtmlRouter);
+app.use('/gallery', cors, passport.authenticate('jwt', { session: false }), galleryRouter);
+app.use('/upload', cors, passport.authenticate('jwt', { session: false }), galleryRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server is running at http://${hostname}:${port}/`);
